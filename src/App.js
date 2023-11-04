34e1797c52b5c3f972242d047a9645b03eb63f75
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2023-11-04 22:50:39 +0700

    finish layouting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
-import { Cover } from './components/cover/Cover';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import "animate.css";
+
+import { Hero } from "./components/hero/Hero";
+import { Cover } from "./components/cover/Cover";
+import { Welcome } from "./components/welcome/Welcome";
 
 function App() {
+  const [click, setClick] = useState(true);
+  const audio = useRef(null);
+
+  const handleClick = () => {
+    setClick(false);
+    if (audio.current) audio.current.play();
+  };
+
   return (
-    <>
-      <Cover />
-    </>
+    <div className="app">
+      <Hero />
+      {click ? (
+        <Cover
+          handleClick={handleClick}
+          className="animate__animated animate__fadeoutUp"
+        />
+      ) : (
+        <Welcome className="animate__animated animamte__fadeIn" />
+      )}
+      <audio ref={audio}>
+        <source src="./assets/music.mp3" type="audio/mpeg" />
+      </audio>
+    </div>
   );
 }
 
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./hero.css";
+
+export const Hero = () => {
+  return (
+    <div className="content-hero">
+      <p className="title ">the wedding of</p>
+      <h1 className="name ">Nailal and Via</h1>
+      <p className="text ">
+        “And I knew exactly how old Walt Disney’s Cinderella felt when she found
+        her prince.”
+        <br />— Elizabeth Young
+      </p>
+    </div>
+  );
+};
